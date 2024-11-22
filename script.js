7cfb1d787d9a0de849c54be043c150399d862cf0
Michael Sandoval <avetravolta@gmail.com>
2024-11-22 09:09:48 -0800

    Added edit button and shortened JS

--- script.js
+++ script.js
@@ -1,49 +1,45 @@
 $(document).ready(function () {
-  //Moment.js code for current date and time
-  let NowMoment = moment().format("MMMM Do YYYY");
-  let displayDate = document.getElementById("currentDay");
-  displayDate.innerHTML = NowMoment;
   let currentHour = moment().format("HH");
 
-  // Button function to clear local storage and clear contents
-  $("#clearFieldsBtn").click(function (event) {
-    event.preventDefault;
-    $("textarea").val("");
-    localStorage.clear();
-  });
-
-  //grabs hour from each time slot and compares it to actual time
+  //grabs saved events from localStorage when the page loads
   $(".time-div").each(function () {
-    var timeDiv = $(this).attr("id").split("-")[1];
-    
-    if (currentHour == timeDiv) {
-      $(this).addClass("present");
-      $(this).children(".description").addClass("white-text");
-    } else if (currentHour < timeDiv) {
-      $(this).removeClass("present");
-      $(this).addClass("future");
-    } else if (currentHour > timeDiv) {
-      $(this).removeClass("future");
-      $(this).addClass("past");
+    var time = $(this).attr("id").split("-")[1]; //get the hour from the div's id
+    var savedData = JSON.parse(localStorage.getItem(time));
+
+    if (savedData) {
+      $(this).children(".time-block").val(savedData.event);
+      $(this).children(".time-block").addClass("saved"); //option to add a class for saved events
     }
   });
 
-  //grabs values from time and value divs and saves them to local storage
-  $(".saveBtn").click(function (event) {
+  //edit button click handler
+  $(".editBtn").click(function (event) {
     event.preventDefault();
-    var value = $(this).siblings(".time-block").val();
-    var time = $(this).parent().attr("id").split("-")[1];
-    localStorage.setItem(time, value);
+    var timeBlockDiv = $(this).parent(); //get the parent div for this time block
+    var textArea = timeBlockDiv.find(".time-block"); //find the textarea inside this div
+
+    //toggle the textarea to be editable and change button visibility
+    textArea.prop("readonly", false); //make the textarea editable
+    $(this).hide();  //hide the edit button
+    timeBlockDiv.find(".saveBtn").show(); //show the save button
   });
 
-  //retrieves items from local storage and sets them in proper places
-  $("#hour-09 .time-block").val(localStorage.getItem("09"));
-  $("#hour-10 .time-block").val(localStorage.getItem("10"));
-  $("#hour-11 .time-block").val(localStorage.getItem("11"));
-  $("#hour-12 .time-block").val(localStorage.getItem("12"));
-  $("#hour-13 .time-block").val(localStorage.getItem("13"));
-  $("#hour-14 .time-block").val(localStorage.getItem("14"));
-  $("#hour-15 .time-block").val(localStorage.getItem("15"));
-  $("#hour-16 .time-block").val(localStorage.getItem("16"));
-  $("#hour-17 .time-block").val(localStorage.getItem("17"));
+  //save button click handler
+  $(".saveBtn").click(function (event) {
+    event.preventDefault();
+    var timeBlockDiv = $(this).parent(); //get the parent div for this time block
+    var value = timeBlockDiv.find(".time-block").val(); //get the value from the textarea
+
+    var time = timeBlockDiv.attr("id").split("-")[1]; //get the hour from the div's id
+
+    //save event to localStorage
+    localStorage.setItem(time, JSON.stringify({ event: value }));
+
+    //hide save button and show edit button
+    $(this).hide(); //hide the save button
+    timeBlockDiv.find(".editBtn").show(); //show the edit button
+
+    //make the textarea non-editable again
+    timeBlockDiv.find(".time-block").prop("readonly", true); 
+  });
 });
